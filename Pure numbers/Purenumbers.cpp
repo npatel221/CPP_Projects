ca471f9afbb0ece4ca71ffc5321dd994cc38d29b
nishp763 <npatel221@gmail.com>
2020-10-07 19:10:49 -0400

    collecting T value from the user

--- Pure numbers/Purenumbers.cpp
+++ Pure numbers/Purenumbers.cpp
@@ -1,8 +1,19 @@
-#include <iostream> // For User IO
+#include <iostream> // for User IO
+#include <math.h> // for math operations
 
 using namespace std;
 
 int main()
 {
+    // Get # of test cases from the User. T value
+    int numTestCases;
+    cin >> numTestCases;
+
+    // Check if the input is within the limits as required
+    if ((numTestCases < 1) or (numTestCases > pow(10, 6)))
+    {
+        return 1; // Terminate execution with an error
+    }
+
     return 0;
 }
