0bb8519495a9663ec86759e439ae31d0770ab08e
nishp763 <npatel221@gmail.com>
2020-10-10 18:02:08 -0400

    Added isEven and checkDigits function

--- Pure numbers/Purenumbers.cpp
+++ Pure numbers/Purenumbers.cpp
@@ -1,11 +1,30 @@
 #include <iostream> // for User IO
 #include <math.h> // for math operations
+#include <sstream> // for string manipulation and conversion
 
 using namespace std;
 
+// This function checks if the number of digits are even or odd. 
+// If even return true, odd return false
 bool isEven(int num)
 {
-    
+    string temp = to_string(num); // convert num to string
+    return ((temp.size()%2) == 0); // check size divisible by 2 to see if even/odd
+}
+
+// This function checks if the input num only consists of digits 4 and 5
+// Return True: if only 4 and 5 present, False: otherwise
+bool checkDigits(int num)
+{
+    string temp = to_string(num); // convert num to string
+    for (int i = 0; i < temp.size(); i++) // loop through the string character by character
+    {
+        if ((temp[i] != '4') && (temp[i] != '5')) // check for 4 and 5 presence
+        {
+            return false;
+        }
+    }
+    return true; // only 4 and 5 are present
 }
 
 int main()
@@ -20,6 +39,6 @@ int main()
         return 1; // Terminate execution with an error
     }
 
-
+    cout << checkDigits(4545) << endl;
     return 0;
 }
