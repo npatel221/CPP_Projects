9444ca49695c681a268b84d82773a2c5a384c40a
nishp763 <npatel221@gmail.com>
2021-01-17 14:29:01 -0500

    started working on toaster implementation

---
+++ Toaster/toaster.cpp
@@ -0,0 +1,13 @@
+/*
+This program implements a software controller for a bread & bagel toaster.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello world" << endl;
+    return 0;
+}
