af1967698ce69a374cbcadaefa13c73247e2149e
nishp763 <npatel221@gmail.com>
2021-01-25 23:02:01 -0500

    added binary to decimal conversion

--- DecToBinary/dectobin.cpp
+++ DecToBinary/dectobin.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int convert(unsigned short myint)
 {
-    //cout << "Hello World\n";
     int power = floor(log(myint) / log(2));
     if (pow(2,power) == myint)
     {
@@ -28,9 +27,42 @@ int convert(unsigned short myint)
     }
 }
 
+unsigned int binToDec(const string binaryNum)
+{
+    unsigned int decimal, exponent;
+    int stringSize = binaryNum.size();
+
+    // iterate through the string
+    for (int i = 0; i < stringSize; i++)
+    {
+        if (binaryNum[i] == '1') // calculate the 2^exponent and add to decimal value
+        {
+            decimal += pow(2,(stringSize - (i+1)));
+        }
+        else if (binaryNum[i] == '0') // do nothing
+        {
+            continue;
+        }
+        else // invalid character
+        {
+            cout << "Invalid binary string" << endl;
+            exit(1);
+        }
+    }
+    return decimal;
+}
 
 int main()
 {
-    cout << "Return: " << convert(9) << endl;
+    // Get how many 1's in a decimal number
+    cout << "---COUNT HOW MANY 1's IN A DECIMAL NUM\n";
+    cout << "Decimal: " << 9 << "\t 1's Count: ";
+    cout << convert(9) << endl;
+
+    // Binary to Decimal demo
+    cout << "---BINARY TO DECIMAL CONVERTER---\n";
+    string binarynum = "011";
+    cout << "Binary: " << binarynum << "\tDecimal: ";
+    cout << binToDec(binarynum) << endl;
     return 0;
 }
